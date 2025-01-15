be0313ab8ec5452e918cf26dc1f0593cfcf59440
Lucas Daniel <luc4xssdanield@gmail.com>
2025-01-14 21:29:00 -0300

    MUDANÇA PARA LISTA DE TAREFAS

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-const botao=document.querySelector("#botao"),select=document.querySelector("#options"),input=document.querySelector("#caixa");botao.addEventListener("click",()=>{select.innerHTML="";let a=input.value;if(a)for(let b,c=0;10>=c;c++)b=document.createElement("option"),select.appendChild(b),b.value=`OPÇÃO ${c}`,b.innerHTML=`${a} X ${c} = ${a*c}`});
+const InputTarefa = document.querySelector("#tarefa")
+const BotaoTarefa = document.querySelector("#botao")
+const ContainerTarefa = document.querySelector(".tarefas")
+
+BotaoTarefa.addEventListener('click', () => {
+    if (!InputTarefa.value){
+        InputTarefa.classList.add("error")
+        return
+    }
+    
+    let NovaTarefa = document.createElement('div')
+    NovaTarefa.classList.add('caixa')
+    NovaTarefa.innerHTML = InputTarefa.value
+
+    let DeleteTarefa = document.createElement('button') 
+    DeleteTarefa.classList.add('delete')
+
+    let DeleteIcone = document.createElement('i')
+    DeleteIcone.classList.add('fa-solid')
+    DeleteIcone.classList.add('fa-trash')
+
+    ContainerTarefa.appendChild(NovaTarefa)
+    NovaTarefa.appendChild(DeleteTarefa)
+    DeleteTarefa.appendChild(DeleteIcone)
+
+    DeleteTarefa.addEventListener('click', () => {
+        if (confirm("A tarefa excluída não poderá ser recuperada. Tem certeza disso?")) {
+            DeleteTarefa.parentNode.remove(); // Remove o elemento pai de DeleteTarefa
+        }
+    })
+})
+
